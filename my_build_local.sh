61b04d48ab0cb266e4a2ea122e3c05c4f11c59c1
Matthew Feickert <matthew.feickert@cern.ch>
2024-02-18 02:55:20 -0600

    Increase CPU for build

--- my_build_local.sh
+++ my_build_local.sh
@@ -9,7 +9,7 @@ mkdir -p build_artifacts/{noarch,linux-64}/
 export CONFIG=linux64
 export DOCKER_IMAGE=quay.io/condaforge/linux-anvil-cos7-x86_64
 export AZURE=False
-export CPU_COUNT=14
+export CPU_COUNT=16
 .scripts/run_docker_build.sh | tee build.log
 
 # docker system prune -f
